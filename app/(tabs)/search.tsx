0328fc278fb92a94d3aee2887d3a9d5069002f45
Smash42 <228090741+Smash42@users.noreply.github.com>
2026-02-24 10:04:02 -0500

    mealcard

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -5,7 +5,7 @@ const background = require("../../assets/images/oceanFade.png");
 const search = () => {
   return (
     <ImageBackground source={background} style={{ flex: 1 }} resizeMode="cover">
-      <View className="flex-1 items-center bg-amber-200">
+      <View className="flex-1 items-center">
         <Text className="text-5xl text-title text-center font-bold mt-20">
           Kitchen Helper
         </Text>
