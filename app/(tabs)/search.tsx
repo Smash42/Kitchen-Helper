5366551864a079299150fe79b60572fc40d6fc62
Smash42 <228090741+Smash42@users.noreply.github.com>
2026-03-20 08:39:00 -0400

    search

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,20 +1,40 @@
+import { Link } from "expo-router";
+import { useState } from "react";
 import { Image, ImageBackground, Text, View } from "react-native";
+import { mealData } from "../data/mealData";
 const sharkchef = require("../../assets/images/Shark-Chef-small.png");
 const background = require("../../assets/images/bgocean.png");
 
 const search = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredMeals = mealData.filter((meal) =>
+    meal.title.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
-    <ImageBackground source={background} style={{ flex: 1 }} resizeMode="cover">
+    <ImageBackground
+      source={background}
+      className="flex-1 z-0"
+      resizeMode="cover"
+    >
       <View className="flex-1 items-center">
         <Text className="text-5xl text-title text-center font-bold mt-20">
           Kitchen Helper
         </Text>
-        <Text></Text>
         <Image
           source={sharkchef}
           className="mx-auto justify-center mt-50"
         ></Image>
-        <Text className="text-xl"> Search Meal Function coming soon</Text>
+
+        {filteredMeals.map((meal) => (
+          <Link key={meal.id} href={`/meal/${meal.id}`} className="mt-5">
+            <Text className="text-2xl text-meals p-2">{meal.title}: </Text>
+            <Text className="text-yellow-100 text-lg">
+              {meal.meatType} - {meal.cookingStyle}
+            </Text>
+          </Link>
+        ))}
       </View>
     </ImageBackground>
   );
